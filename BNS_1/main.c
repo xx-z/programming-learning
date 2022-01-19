9ce63704eefcdc357d197f9a77d7cb93a16daa50
XYX_Y <46470939+XYX-Y@users.noreply.github.com>
2022-01-19 10:39:22 +0900

    マップの生成

--- BNS_1/main.c
+++ BNS_1/main.c
@@ -1,20 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int MaxKey = 5;
-int KeyArr[30] = { 0 };         //全てのキー入力
-int GetKeyCount = 0;                  //
+#define MAXKEY 20                                //最大キー入力回数
+#define MAXMAP 20                             //最大マップサイズ
 
+int KeyList[MAXKEY] = { 0 };                  //キー入力リスト
+int GetKeyCount = 0;                             //キー入力回数
+
+char MapList[MAXMAP][MAXMAP];
+int MapSizeX, MapSizeY = 0;
+
+int PlayerPosX, PlayerPosY = 0;
+
+//マップの読み込み
 void MapLoad() {
+    const char* file = "map_1.txt";
+    FILE* fp;
+    char c;
+    int X = 0;
+    int Y = 0;
+
+    fopen_s(&fp, file, "r");
+    if (fp == NULL) {
+        printf("ファイルエラー\n%sのオープンに失敗しました。\n", file);
+        exit(1);
+    }
+    //	EOFまでファイルから文字を1文字ずつ読み込む
+    while ((c = fgetc(fp)) != EOF) {
+        if ('\n' == c) {
+            if (MapSizeX < X) {
+                MapSizeX = X;
+            }
+            Y++; X = 0;
+        }
+        else {
+            MapList[X][Y] = c;
+            X++; 
+        }
+    }
+    MapSizeY = Y + 1;
+    fclose(fp);
+}
+
+//マップの表示
+void MapDisplay() {
+    //printf("%d, %d\n", MapSizeX, MapSizeY);
+    printf("\n");
+    //本マップ生成
+    //0:off   1:on   2:start   3:wall
+    for (int i = 0; i < MapSizeY; i++) {
+        for (int j = 0; j < MapSizeX; j++) {
+            if ('0' == MapList[j][i]) {
+                printf("□");
+            } else if ('2' == MapList[j][i]){
+                PlayerPosX = j; PlayerPosY = i;
+                MapList[j][i] = '1';
+                printf("\x1b[44m　\x1b[m");
+            } else {
+                printf("\x1b[47m　\x1b[m");
+            }
+        }
+        printf("\n");
+    }
 
 }
 
+//キーリストにキーを代入する関数
 void KeyAdd(Key) {
-    KeyArr[GetKeyCount] = Key;
+    KeyList[GetKeyCount] = Key;
 }
 
+//キー入力検知
 void KeyGet() {
     int Key = 0;
-    for (GetKeyCount = 0; GetKeyCount < MaxKey; GetKeyCount++) {
+    for (GetKeyCount = 0; GetKeyCount < MAXKEY; GetKeyCount++) {
         switch (getch()) {
             case 0xe0:
                 switch (getch()) {
@@ -26,23 +85,45 @@ void KeyGet() {
                 KeyAdd(Key);
                 break;
             case 0x0d: 
-                printf("enter"); 
-                GetKeyCount = MaxKey;
+                printf("\n"); 
+                GetKeyCount = MAXKEY;
                 break;
         }
     }
 }
 
-void main() {
-    MapLoad();
-    KeyGet();
-    printf("\n////////////////////////////\n");
-    for (int i = 0; i < 30; i++) {
-        printf("%d", KeyArr[i]);
+void Execution() {
+    //printf("%d%d", PlayerPosX, PlayerPosY);
+    for (int i = 0; i < MAXKEY; i++) {
+        while (MapList[PlayerPosX][PlayerPosY] != '3') {
+            switch (KeyList[i]) {
+                case 1: PlayerPosY--; break;
+                case 2: PlayerPosX++; break;
+                case 3: PlayerPosY++; break;
+                case 4: PlayerPosX--; break;
+            }
+        } 
+        if (MapList[PlayerPosX][PlayerPosY] == '3') {
+            switch (KeyList[i]) {
+                case 1: PlayerPosY++; break;
+                case 2: PlayerPosX--; break;
+                case 3: PlayerPosY--; break;
+                case 4: PlayerPosX++; break;
+            }
+        }
+        printf(", %d%d", PlayerPosX, PlayerPosY);
     }
 }
 
+void main() {
+    MapLoad();
+    MapDisplay();
+    printf("\n＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\n\n");
+    KeyGet();
+    Execution();
+}
+
 /*
-printf("\x1b[41m□□□□■■■■\x1b[m\n"); 
+printf("\x1b[41m　\x1b[m\n"); 
 printf("\x1b[34m□□□□■■■■\033[m\n");
 */
