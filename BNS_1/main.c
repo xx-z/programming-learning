7f64740dd8f36fa5355b083c52ebaa6f5975c732
XYX_Y <46470939+XYX-Y@users.noreply.github.com>
2022-01-20 17:37:49 +0900

    プレーヤーの移動の反映

--- BNS_1/main.c
+++ BNS_1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <Windows.h>
 
-#define MAXKEY 5                                      //最大キー入力回数
+#define MAXKEY 30                                      //最大キー入力回数
 #define MAXMAP 20                                   //最大マップサイズ
 
 int KeyList[MAXKEY] = { 0 };                        //キー入力リスト
@@ -43,7 +43,6 @@ void MapLoad() {
 
 //マップの表示
 void MapDisplay() {
-    //printf("%d, %d\n", MapSizeX, MapSizeY);
     printf("\n");
     //本マップ生成
     //0:off   1:on   2:start   3:wall
@@ -51,6 +50,8 @@ void MapDisplay() {
         for (int j = 0; j < MapSizeX; j++) {
             if ('0' == MapList[j][i]) {
                 printf("□");
+            } else if ('1' == MapList[j][i]) {
+                printf("\x1b[31m■\x1b[m");
             } else if ('2' == MapList[j][i]){
                 PlayerPosX = j; PlayerPosY = i;
                 MapList[j][i] = '1';
@@ -61,7 +62,6 @@ void MapDisplay() {
         }
         printf("\n");
     }
-
 }
 
 //キーリストにキーを代入する関数
@@ -72,25 +72,44 @@ void KeyAdd(Key) {
 //キー入力検知
 void KeyGet() {
     int Key = 0;
-    for (GetKeyCount = 0; GetKeyCount < MAXKEY; GetKeyCount++) {
+    int Count = 0;
+
+    while (GetKeyCount != MAXKEY) {
         switch (getch()) {
-            case 0xe0:
-                switch (getch()) {
-                    case 0x48: Key = 1; printf("↑"); break;
-                    case 0x4d: Key = 2; printf("→"); break;
-                    case 0x50: Key = 3; printf("↓"); break;
-                    case 0x4b: Key = 4; printf("←"); break;
-                }
-                KeyAdd(Key);
-                break;
-            case 0x0d: 
-                printf("\n"); 
-                //Count = GetKeyCount;
-                GetKeyCount = MAXKEY;
-                break;
+        case 0xe0:
+            switch (getch()) {
+            case 0x48: Key = 1; printf("↑"); break;
+            case 0x4d: Key = 2; printf("→"); break;
+            case 0x50: Key = 3; printf("↓"); break;
+            case 0x4b: Key = 4; printf("←"); break;
+            }
+            KeyAdd(Key);
+            GetKeyCount++; Count++;
+            break;
+        case 0x0d:
+            Count = GetKeyCount;
+            GetKeyCount = MAXKEY;
+            break;
         }
     }
-    printf("%d\n", GetKeyCount);
+    GetKeyCount = Count;
+    printf("\n");
+    //printf("%d\n", GetKeyCount);
+}
+
+void setCursorPos(int x, int y)
+{
+    HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
+    SetConsoleCursorPosition(hCons, pos);
+}
+
+void ExecutionDisplay() {
+    setCursorPos(0, 0);
+    MapDisplay();
+    Sleep(1000);
 }
 
 void Execution() {
@@ -103,6 +122,10 @@ void Execution() {
                 case 3: PlayerPosY++; break;
                 case 4: PlayerPosX--; break;
             }
+            if (MapList[PlayerPosX][PlayerPosY] != '3') {
+                MapList[PlayerPosX][PlayerPosY] = '2';
+                ExecutionDisplay();
+            }
         } 
         if (MapList[PlayerPosX][PlayerPosY] == '3') {
             switch (KeyList[i]) {
@@ -112,9 +135,11 @@ void Execution() {
                 case 4: PlayerPosX++; break;
             }
         }
-        printf(", %d%d", PlayerPosX, PlayerPosY);
+        //printf(", %d%d", PlayerPosX, PlayerPosY);
     }
 }
+
+
 
 void main() {
     MapLoad();
